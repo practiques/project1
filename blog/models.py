0b5597ada4be9a2dab788623e98809a19f78988a
Juan Guirao <juan.guirao@vass.es>
2013-10-16 12:02:47 -0700

    Començats els models

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,10 @@
 from django.db import models
 from django.utils.timezone import utc
 import datetime
+
+class Post(model.Models):
+	titol=models.CharField(max_lenght=100)
+	entradeta
+	cos
+	data_creacio
+	data_publicacio
